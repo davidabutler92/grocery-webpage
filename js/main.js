41c95d7f05411175d0123fc9201abf13198ba733
davidabutler92 <davidabutler92@yahoo.com>
2020-08-17 15:52:55 -0700

    add form submit function and store user input in 3 variables (item1 item2 item3)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+  $("#list").submit(function(event) {
+    event.preventDefault();
+
+    const item1 = $("#item1").val();
+    const item2 = $("#item2").val();
+    const item3 = $("#item3").val();
+    // console.log(item1, item2, item3);
+  });
 });
