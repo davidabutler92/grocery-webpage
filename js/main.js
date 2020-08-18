3eafb30521be5139a36e4098719e7fc85d2b1ef0
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 13:08:42 -0700

    add change forEach into a for loop

--- js/main.js
+++ js/main.js
@@ -5,22 +5,24 @@ $(document).ready(function() {
     const item1 = $("#item1").val();
     const item2 = $("#item2").val();
     const item3 = $("#item3").val();
-    // console.log(item1, item2, item3);
+    
 
     const groceryItems = [item1, item2, item3];
     groceryItems.sort();
-    // console.log(groceryItems);
+    console.log(groceryItems);
 
-    const groceryList = document.createElement('ul');
-    // console.log(groceryList);
-    groceryItems.forEach(function(groceryItem) {
-      let li = document.createElement('li');
-      li.textContent = groceryItem;
-      list.appendChild(li);
-      const insert = document.querySelector("#insert");
-      insert.appendChild(list);
-      console.log(insert);
-    });
+    for (i = 0; i < groceryItems.length; i++) {
+      $("#list").append("<li>" + groceryItems)
+    }
+    
+    // groceryItems.forEach(function(groceryItem) {
+    //   let li = document.createElement('li');
+    //   li.textContent = groceryItem;
+    //   list.appendChild(li);
+    //   const insert = document.querySelector("#insert");
+    //   insert.appendChild(list);
+    //   console.log(insert);
+    // });
 
   });
 });
